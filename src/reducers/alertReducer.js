963b92fe31257ef7bed8649ebd11d750c903cde3
mateokeller <mateokellergms@gmail.com>
2022-11-10 11:05:51 -0300

    fix warning no anonymous default export

--- src/reducers/alertReducer.js
+++ src/reducers/alertReducer.js
@@ -6,6 +6,7 @@ const initialState = {
   alert: null,
 };
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default function (state = initialState, action) {
   switch (action.type) {
     case DISPLAY_ALERT:
